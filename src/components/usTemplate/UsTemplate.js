3598f710b387202c126823573d9c448d26b02839
Alma Suarez <asuarez@checkintocash.com>
2022-11-01 18:22:21 -0600

    types

---
+++ src/components/usTemplate/UsTemplate.js
@@ -0,0 +1,23 @@
+import React, { Fragment} from 'react'
+import { Link } from "gatsby"
+
+export default function UsTemplate({wordpress, flag, state}) { 
+    let id_prov = 0;
+    const searchID =(id) => {
+        const num = +id;
+        return wordpress.filter(moment => moment.databaseId === num);
+    }
+  return (
+    <section style={{width: "20%" }}>
+        <h3>{state.state}</h3>
+        { 
+            flag.map( (detail, index)=> {
+                 id_prov = searchID(detail.id); 
+                return(
+                     <Link to={`${id_prov[0].uri}`}  key={`${state.state}-${index}`}><li>{detail.locality}, {detail.name}</li> </Link>
+                )
+            })
+        }
+    </section>
+  )
+}
--- src/pages/us.js
+++ src/pages/us.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState,useCallback } from 'react'
 import { Link,graphql } from "gatsby"
+import { statesList } from '../selectors/getType';
+import UsTemplate from '../components/usTemplate/UsTemplate';
 
-export default function us({data}) {
-  console.log(data);
+export default function US({data}) { 
+   const [states, setStates] = useState([]);
+   useEffect(() => {
+    const arr = statesList()
+    setStates(arr);  
+   }, [])
+  const moment = data.allMomentFeed.nodes;
+  const wordpress = data.allWpPage.nodes;   
+  const filterState =(state) => { 
+    return moment.filter(moment_state => moment_state.state_min === state);
+  }
+
+  /**/
+  
+   const SearchState = useCallback(() => {
+    let flag = ""; 
+    console.log("running ");
+    return(
+      states.map( (state, index) => {
+        flag = filterState(state.state)
+        if(flag.length > 0 ){
+          return(
+            <UsTemplate key={`us-${index}`} wordpress={wordpress} flag={flag} state={state} />
+          )
+        }
+      })
+    )
+  }, [states ]); 
+  
+ 
   return (
-    <div>US TEMPLATE <br /> here goes the full list</div>
+    <Fragment>
+    {
+         ( states.length > 0 ) ? ( SearchState() )  : <></>
+    } 
+    </Fragment>
   )
 }
 
@@ -24,6 +58,20 @@ query USQuery {
         templateName
       }
       uri
+    }
+  }
+  allMomentFeed {
+    nodes {
+      address
+      id
+      locality
+      momentfeed_id
+      name
+      post_name
+      state
+      state_min
+      status
+      zip
     }
   }
 }  
